f4573593d96cb3375298ab89d476cb2c0c5ffcc9
Léo Belini <leobelini96@gmail.com>
2025-08-09 17:39:42 -0300

    feat: use form bot

--- frontend/src/pages/system/index.tsx
+++ frontend/src/pages/system/index.tsx
@@ -3,14 +3,27 @@ import NewBot from "./new-bot"
 import { useSystemContext } from "./context"
 import { Label } from "@/components/ui/label"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Separator } from "@/components/ui/separator"
+import type { ConfigFileBot } from "@/types/config-file"
+import { useCallback, useState } from "react"
+import FormBot from "./form-bot"
 
 function System() {
   const { config, createBot } = useSystemContext()
+
+  const [currentBot, setCurrentBot] = useState<ConfigFileBot>(null!);
+
+  const handleSelectBot = useCallback((botId: string) => {
+    const bot = config?.bots?.find(bot => bot.id === botId);
+    if (!bot) return;
+    setCurrentBot(bot);
+  }, [config]);
+
   return (
-    <div className="flex h-screen flex-col w-screen">
+    <div className="flex h-screen flex-col w-screen px-5 py-2">
       <div className="flex w-full items-center justify-center gap-2 p-2">
         <Label id="bot">Bot:</Label>
-        <Select>
+        <Select onValueChange={handleSelectBot}>
           <SelectTrigger className="w-[280px]">
             <SelectValue placeholder="Selecione um bot" />
           </SelectTrigger>
@@ -20,9 +33,13 @@ function System() {
             ))}
           </SelectContent>
         </Select>
-        <NewBot  handleCreate={createBot} />
+        <NewBot handleCreate={createBot} />
 
       </div>
+
+      <Separator className="my-2"/>
+
+      {currentBot && <FormBot bot={currentBot} isPreview  copyButtons/>}
     </div>
   )
 }
