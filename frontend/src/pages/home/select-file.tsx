32ff5c989a828530228fe578aae535ec6658c0b4
Léo Belini <leobelini96@gmail.com>
2025-08-12 17:12:20 -0300

    feat: change select file

--- frontend/src/pages/home/select-file.tsx
+++ frontend/src/pages/home/select-file.tsx
@@ -1,6 +1,5 @@
 import { z } from "zod"
 import { toast } from "sonner"
-import { CogIcon } from "lucide-react"
 import { useNavigate } from "react-router"
 import { useCallback, useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -11,8 +10,8 @@ import { Button } from "@/components/ui/button"
 import { useSystemContext } from "../system/context"
 import { GetPathFile } from '../../../wailsjs/go/main/App';
 import { frontend, main } from "../../../wailsjs/go/models";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 
 const formSchema = z.object({
   password: z.string().min(1, "Informe uma senha"),
@@ -50,91 +49,72 @@ function SelectFile() {
   }, [])
 
   const handleSetFieldFile = useCallback(
-      async (name: FieldPath<FormData>) => {
-        try {
-          const configMap: Partial<Record<FieldPath<FormData>, main.ReplaceFileConfigParam>> = {
-            filePath: new main.ReplaceFileConfigParam({
-              Title: "Selecione o arquivo",
-              Filter: [
-                new frontend.FileFilter({
-                  DisplayName: "Arquivo de configuração",
-                  Pattern: "*.start-openkore-latam",
-                }),
-              ],
-            }),
-            
-          };
-  
-          const params = configMap[name];
-          if (!params) throw new Error("Nenhuma opção selecionada");
-  
-          const value = await GetPathFile(params);
-          form.setValue(name, value);
-        } catch (error) {
-          const message =
-            error instanceof Error
-              ? error.message
-              : "Não foi possível selecionar o arquivo";
-          toast(message);
-        }
-      },
-      [form, toast]
-    );
+    async (name: FieldPath<FormData>) => {
+      try {
+        const configMap: Partial<Record<FieldPath<FormData>, main.ReplaceFileConfigParam>> = {
+          filePath: new main.ReplaceFileConfigParam({
+            Title: "Selecione o arquivo",
+            Filter: [
+              new frontend.FileFilter({
+                DisplayName: "Arquivo de configuração",
+                Pattern: "*.start-openkore-latam",
+              }),
+            ],
+          }),
+
+        };
+
+        const params = configMap[name];
+        if (!params) throw new Error("Nenhuma opção selecionada");
+
+        const value = await GetPathFile(params);
+        form.setValue(name, value);
+        setOpen(true);
+      } catch (error) {
+        const message =
+          error instanceof Error
+            ? error.message
+            : "Não foi possível selecionar o arquivo";
+        toast(message);
+      }
+    },
+    [form, toast]
+  );
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button variant="outline">Selecionar configuração</Button>
-      </DialogTrigger>
-      <DialogContent>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
-            <DialogHeader>
-              <DialogTitle>Seleção de configuração</DialogTitle>
-            </DialogHeader>
-            <FormField
-                  control={form.control}
-                  name="filePath"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Arquivo de configuração<span className="text-destructive">*</span></FormLabel>
-                      <div className="flex gap-2">
-                        <FormControl>
-                          <Input disabled {...field} />
-                        </FormControl>
-                        <Button type="button" size="icon" onClick={() => handleSetFieldFile("filePath")}><CogIcon /></Button>
-                      </div>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Senha de acesso<span className="text-destructive">*</span></FormLabel>
-                  <FormControl>
-                    <Input type="password" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    Essa senha será usada criptografar as informações de acesso e solicitar sempre que abrir.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <DialogFooter className="flex flex-row gap-2 justify-end">
-              <DialogClose asChild>
-                <Button variant="outline">Cancel</Button>
-              </DialogClose>
-              <Button type="submit">Abrir</Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
+    <>
+      <Button variant="outline" onClick={() => handleSetFieldFile("filePath")}>Selecionar configuração</Button>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+              <DialogHeader>
+                <DialogTitle>Senha de acesso</DialogTitle>
+              </DialogHeader>
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha de acesso<span className="text-destructive">*</span></FormLabel>
+                    <FormControl>
+                      <Input type="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <DialogFooter className="flex flex-row gap-2 justify-end">
+                <DialogClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogClose>
+                <Button type="submit">Abrir</Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </>
   )
 }
 
